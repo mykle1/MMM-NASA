0424957717ca30b2bc9e593cecb5352f0c009213
mykle1 <themykle@imykle.com>
2017-05-31 20:33:17 -0400

    Add files via upload

--- MMM-NASA.js
+++ MMM-NASA.js
@@ -2,22 +2,22 @@
  * Module: MMM-NASA
  *
  * By Mykle1
- *
+ * MIT License
  */
 Module.register("MMM-NASA", {
 
     // Module config defaults.
     defaults: {
-        search: "nebula",             // See NASA Search List in ReadMe  
-		rotateInterval: 5 * 1000, // New Image Appears
-		useHeader: false,
+        search: "nebula", // See Search List in ReadMe  
+        rotateInterval: 5 * 60 * 1000, // New Image Appears
+        useHeader: false,
         header: "",
-		maxWidth: "195px",             // adjust to your liking 
-		animationSpeed: 3000,          // Image fades in and out
+        maxWidth: "195px", // adjust to your liking 
+        animationSpeed: 3000, // Image fades in and out
         initialLoadDelay: 4250,
         retryDelay: 2500,
-		updateInterval: 60 * 60 * 1000, // 60 minutes. No need to change!
-		
+        updateInterval: 60 * 60 * 1000, // NASA limitation = 50 calls per day. Do NOT change!
+
     },
 
     getStyles: function() {
@@ -31,8 +31,7 @@ Module.register("MMM-NASA", {
 
         // Set locale.
         this.url = "https://images-api.nasa.gov/search?q=" + this.config.search + "&media_type=image";
-        //this.collection = {};  // collection is from the data path
-	    this.nasa = {};        // beacause this.nasa=data.items ;)
+        this.nasa = {};
         this.activeItem = 0;
         this.rotateInterval = null;
         this.scheduleUpdate();
@@ -57,62 +56,58 @@ Module.register("MMM-NASA", {
             wrapper.appendChild(header);
         }
 
-        // should I use collection or nasa here????  collection from data path
-        var keys = Object.keys(this.nasa); 
+
+        var keys = Object.keys(this.nasa);
         if (keys.length > 0) {
             if (this.activeItem >= keys.length) {
                 this.activeItem = 0;
             }
-
-           // again, I don't know if I should use nasa or collection
             var nasa = this.nasa[keys[this.activeItem]];
-   
-            
-            for(var i = 0; i < nasa.links.length; i++){ 
-			var nasalinks = nasa.links[i];
-			
-console.log(nasalinks);
-console.log(nasa.data[0].description);    
-            var top = document.createElement("div");
-            top.classList.add("list-row");
-            
-            var title = document.createElement("div");
-           
-			title.classList.add("xsmall", "bright");
-			title.innerHTML = nasa.data[0].title;
-			wrapper.appendChild(title);
-			// Picture
-			var nasaLogo = document.createElement("div");
-			var nasaIcon = document.createElement("img");
-			nasaIcon.classList.add("list-left", "photo"); 
-			nasaIcon.src = nasalinks.href;  // path from data
-				
-			nasaLogo.appendChild(nasaIcon);
-			wrapper.appendChild(nasaLogo);
-			
-			var nasaDes = document.createElement("div");
-			nasaDes.classList.add("xsmall", "bright");
-			nasaDes.innerHTML = this.sTrim(nasa.data[0].description, 125, ' ', ' ...');
-			wrapper.appendChild(nasaDes);
-				}
+
+
+            for (var i = 0; i < nasa.links.length; i++) {
+                var nasalinks = nasa.links[i];
+
+
+                var top = document.createElement("div");
+                top.classList.add("list-row");
+
+
+                var title = document.createElement("div");
+                title.classList.add("small", "bright");
+                title.innerHTML = nasa.data[0].title;
+                wrapper.appendChild(title);
+
+
+                var nasaLogo = document.createElement("div");
+                var nasaIcon = document.createElement("img");
+                nasaIcon.classList.add("list-left", "photo");
+                nasaIcon.src = nasalinks.href;
+                nasaLogo.appendChild(nasaIcon);
+                wrapper.appendChild(nasaLogo);
+
+
+                var nasaDes = document.createElement("div");
+                nasaDes.classList.add("xsmall", "bright");
+                nasaDes.innerHTML = this.sTrim(nasa.data[0].description, 187, ' ', ' ...');
+                wrapper.appendChild(nasaDes);
+            }
         }
         return wrapper;
     },
 
     processNASA: function(data) {
-        //no such thing
-		//this.nasa = data.nasa;
-       this.nasa=data.items
+        this.nasa = data.items
         this.loaded = true;
     },
-    
-    sTrim: function (str, length, delim, appendix) {
-    if (str.length <= length) return str;
-    var trimmedStr = str.substr(0, length+delim.length);
-    var lastDelimIndex = trimmedStr.lastIndexOf(delim);
-    if (lastDelimIndex >= 0) trimmedStr = trimmedStr.substr(0, lastDelimIndex);
-    if (trimmedStr) trimmedStr += appendix;
-    return trimmedStr;
+
+    sTrim: function(str, length, delim, appendix) {
+        if (str.length <= length) return str;
+        var trimmedStr = str.substr(0, length + delim.length);
+        var lastDelimIndex = trimmedStr.lastIndexOf(delim);
+        if (lastDelimIndex >= 0) trimmedStr = trimmedStr.substr(0, lastDelimIndex);
+        if (trimmedStr) trimmedStr += appendix;
+        return trimmedStr;
     },
 
     scheduleCarousel: function() {
